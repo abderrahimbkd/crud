4c01f6acf3bfabc836f5a560250a310251975afc
abderrahimbkd <80970245+abderrahimbkd@users.noreply.github.com>
2024-12-05 14:39:21 +0100

    first commit

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,55 @@
+import { Link, Outlet } from "react-router-dom";
+
+import { Navigate } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
+
+export default function DefaultLayout() {
+    const { user, token, setUser, setToken, notification } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
+
+    const onLogout = (ev) => {
+        ev.preventDefault();
+        axiosClient.post("/logout").then(() => {
+            setUser({});
+            setToken(null);
+        });
+    };
+
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+
+                    <div>
+                        {user.name} &nbsp; &nbsp;
+                        <a onClick={onLogout} className="btn-logout" href="#">
+                            Logout
+                        </a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+                {notification && (
+                    <div className="notification">{notification}</div>
+                )}
+            </div>
+        </div>
+    );
+}
